c806e95b4594f4ad618df4dfd0a2529b45f7a85a
Marc N <git@marcnitzsche.de>
2022-02-17 12:24:32 +0100

    Add material-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import Head from "next/head";
 
+import { Container, Snackbar, Button, Typography } from "@mui/material";
+
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <Head>
         <title>Audiate</title>
         <meta name="description" content="Annotate audio files" />
       </Head>
-
-      <h1>Hello World</h1>
+      <Container sx={{ marginTop: 2 }}>
+        <Typography variant="h3" gutterBottom>
+          Hello World
+        </Typography>
+        <Button variant="outlined" onClick={() => setOpen(true)}>
+          Click Me
+        </Button>
+      </Container>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={() => setOpen(false)}
+        message="Button Clicked"
+      />
     </>
   );
 }

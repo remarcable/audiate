4a6e927ded7ec4d3f156e7d45b18757c901488b5
Marc N <git@marcnitzsche.de>
2022-02-20 13:48:45 +0100

    Add dropzone to get audio file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./_app.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Head from "next/head";
+import { useDropzone } from "react-dropzone";
 
-import { Container, Snackbar, Button, Typography } from "@mui/material";
+import { Container, Button, Typography, Paper } from "@mui/material";
 
 export default function Home() {
-  const [open, setOpen] = useState(false);
+  const onDrop = useCallback(([audioFile]) => {
+    if (!audioFile) return;
+
+    setFile(audioFile);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive, isDragAccept } =
+    useDropzone({
+      onDrop,
+      multiple: false,
+      accept: "audio/*",
+    });
+
+  const [file, setFile] = useState(null);
 
   return (
     <>
@@ -12,20 +25,35 @@ export default function Home() {
         <title>Audiate</title>
         <meta name="description" content="Annotate audio files" />
       </Head>
-      <Container sx={{ marginTop: 2 }}>
-        <Typography variant="h3" gutterBottom>
-          Hello World
-        </Typography>
-        <Button variant="outlined" onClick={() => setOpen(true)}>
-          Click Me
-        </Button>
+
+      <Container sx={{ mt: 2 }}>
+        <Paper
+          {...getRootProps()}
+          sx={{
+            minHeight: 100,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "text.secondary",
+            cursor: "pointer",
+          }}
+          variant="outlined"
+        >
+          <input {...getInputProps()} />
+          {isDragActive && isDragAccept ? (
+            <Typography variant="body1">Drop the file here ...</Typography>
+          ) : (
+            <Typography variant="body1">
+              Drag and drop an audio file here, or click to select one
+            </Typography>
+          )}
+        </Paper>
+        {file && (
+          <Typography variant="h3" gutterBottom>
+            {file.name}
+          </Typography>
+        )}
       </Container>
-      <Snackbar
-        open={open}
-        autoHideDuration={2000}
-        onClose={() => setOpen(false)}
-        message="Button Clicked"
-      />
     </>
   );
 }

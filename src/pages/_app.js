ddee6449d358875759eff668f6232e4d60762ebe
Marc N <git@marcnitzsche.de>
2022-02-20 15:35:17 +0100

    Use MaterialUI CssBaseline instead of normalize

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,20 @@
-import "normalize.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "./_app.css";
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import theme from "lib/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;

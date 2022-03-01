b03aedb0b61acffe75ba58f151b867a65868be6d
remarcable <remarcable@users.noreply.github.com>
2022-03-01 15:00:04 +0000

    deploy: 2acad508a98275c895424e3b59afcb83ef2452fe

--- _next/static/LI8LonFwPt2-69xg-VGnU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/4-67dd3069f9033eb6.js","static/chunks/pages/index-277f795c4b675673.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
